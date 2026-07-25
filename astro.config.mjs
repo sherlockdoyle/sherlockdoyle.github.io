fc18c98c1c201f89d9ad6f33d2c4d82c6607d676
Shekhar Dutta <sherlockdoyle1904+github@gmail.com>
2026-07-25 16:36:46 +0530

    feat: add JS less endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { parse } from 'node-html-parser';
+import fs from 'node:fs/promises';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    {
+      name: 'nojs-strip-scripts',
+      hooks: {
+        'astro:build:done': async ({ assets }) => {
+          const fileUrl = assets.get('/nojs')?.find(url => url.pathname.endsWith('index.html'));
+          if (!fileUrl) return;
+
+          const root = parse(await fs.readFile(fileUrl, 'utf-8'));
+          root.querySelectorAll('head script:not(:first-of-type),body script').forEach(el => el.remove());
+
+          await fs.writeFile(fileUrl, root.toString(), 'utf-8');
+        },
+      },
+    },
+  ],
+});
